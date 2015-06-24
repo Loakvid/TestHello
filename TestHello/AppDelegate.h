0927492328df3c2cb7bbf7170980ba8e9788ba9e
周鼎 <lokavid@outlook.com>
2015-06-24 19:30:51 +0800

    Initial Commit

---
+++ TestHello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestHello
+//
+//  Created by Lokavid on 15/6/24.
+//  Copyright (c) 2015年 ermiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
